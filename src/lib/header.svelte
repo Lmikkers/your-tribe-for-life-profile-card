c06cb48eb6e81debd0716942161b8b77a8ce4a62
Larissa <larissa.mikkers@hotmail.com>
2024-09-16 14:07:26 +0200

    closed #5
    Linkjes aangevuld!

---
+++ src/lib/header.svelte
@@ -0,0 +1,47 @@
+<script>
+    export let data
+</script>
+
+<header>
+    <nav>
+        <a href="">LM</a>
+        <ul>
+            <li><a href="#">i-love-web</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+
+<style>
+    header {
+        width: 100%;
+    }
+    nav {
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr 10fr 1fr;
+    }
+    nav > a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    ul {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: row;
+        gap: 1rem;
+        width: 100%;
+        list-style: none;
+    }
+    a {
+        text-decoration: none;
+        color: black;
+        font-size: 1.25rem;
+        font-family: var(--mainFont);
+        padding: 1rem;
+        background-color: antiquewhite;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     export let data
+
+    import Header from '$lib/header.svelte';
   
     function checkAvatarImage (avatar) {
       if (avatar.includes('https')) {
@@ -9,51 +11,53 @@
       return false
     }
     console.log(data);
+    
+    import mailIcon from '/assets/mailWhite.svg';
+    import githubIcon from '/assets/github-mark-white.svg';
+    import instaIcon from '/assets/instagramWhite.svg';
+    import turnIcon from '/assets/turnedWhite.svg';
 
-    function flip() {
-        console.log('test')
-		count += 1;
-	}
+    // Variabele om bij te houden of het artikel open is
+    let isFlip = false;
+
+    // Functie om de .flip class toe te voegen
+    const toggleFlip = () => {
+        isFlip = !isFlip;
+        console.log('toggle card', isFlip);
+    };
 
 </script>
 
+<Header />
+
 <main id="cardContainer">
-    <article class="cardFront">
-        <section>
-            <ul>
-                <li><a href="">Mail</a></li>
-                <li><a href="">Github</a></li>
-                <li><a href="">Insta</a></li>
-            </ul>
-    
-            <button on:click={flip}></button>
-        </section>
+    <div class="cards" class:flip={isFlip}>
+        <article class="cardFront">
+            <section>
+                <ul>
+                    <li><a href="mailto:larissa.mikkers@hva.nl"><img src="{mailIcon}" alt="mail icoon"></a></li>
+                    <li><a href="https://github.com/{data.persons.github_handle}" target="_blank"><img src="{githubIcon}" alt="github logo icoon"></a></li>
+                    <li><a href="#"><img src="{instaIcon}" alt="instagram logo"></a></li>
+                </ul>
 
-        <section>
-            <h1>{data.persons.name}</h1>
-            <h2>{data.persons.surname}</h2>
-        </section>
-    </article>
+                <button on:click={toggleFlip}><img src="{turnIcon}" alt="button to go to the back of the card"></button>
+            </section>
 
-    <article class="cardBack">
-        <p>{data.persons.bio}</p>
-    </article>
+            <section>
+                <h1 class="typeWriter">{data.persons.name}</h1>
+                <h2>{data.persons.surname}</h2>
+            </section>
+        </article>
+
+        <article class="cardBack">
+            <button on:click={toggleFlip}><img src="{turnIcon}" alt="button to go to the back of the card"></button>
+            <h3>{data.persons.name}</h3>
+            <p>{data.persons.bio}</p>
+        </article>
+    </div>
 </main>
 
 <style>
-    h1 {
-        font-family: "Expletus Sans", sans-serif;
-        text-transform: uppercase;
-        letter-spacing: 1.2rem;
-        font-size: 3.8rem;
-        font-weight: 500;
-        color: #3797E7;
-    }
-    h2 {
-        font-size: 2rem;
-        text-transform: lowercase;
-        font-weight: 100;
-    }
     main {
         min-height: 100vh;
         display: grid;
@@ -61,6 +65,17 @@
         font-family: "Roboto", sans-serif;
         background: #fbf7f1;
         perspective: 1000px;
+    }
+    h1.typeWriter {
+        overflow: hidden;
+        border-right: 0.15em solid orange;
+        white-space: nowrap;
+        animation: typing 2.25s steps(30, end), blink-caret .5s step-end infinite;
+    }
+    h2 {
+        font-size: 2rem;
+        text-transform: lowercase;
+        font-weight: 100;
     }
     article {
         position: relative;
@@ -122,4 +137,84 @@
         grid-template-columns: repeat(10, 1fr);
         grid-template-rows: repeat(6, 1fr); */
     }
+    button {
+        width: 2.75rem;
+        height: 2.75rem;
+        background: black;
+        border-radius: 50%;
+        border: none;
+        color: white;
+        cursor: pointer;
+        transition: 250ms ease-in-out;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .flip button {
+        position: absolute;
+        top: 2rem;
+        right: 2rem;
+    }
+    button img,
+    a img {
+        width: 1.25em;
+        aspect-ratio: 1 / 1 ;
+        object-fit: contain;
+    }
+    button:hover {
+        transform: rotate(-20deg);
+        background-color: rgb(36, 36, 36);
+    }
+
+    /* wanneer de button wordt geklikt */
+     /* nieuwe code */
+     .cards {
+        width: 40rem;
+        height: 25rem;
+        position: relative;
+        transform-style: preserve-3d;
+        transition: transform 1s;
+    }
+    .cards.flip {
+        transform: rotateY(180deg);
+    }
+    .cardFront, .cardBack {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        backface-visibility: hidden;
+    }
+    .cardFront {
+        transform: rotateY(0deg);
+    }
+    .cardBack {
+        transform: rotateY(180deg);
+    }
+
+    .cardBack h3 {
+        grid-column: 4 / span 4;
+        grid-row: 1/ span 2;
+    }
+    .cardBack > img {
+        position: absolute;
+        top: -3rem;
+        left: -3rem;
+        width: 12rem;
+        height: 12rem;
+        border-radius: 50%;
+        border: 8px solid white;
+        object-fit: cover;
+    }
+    .cardBack p {
+        grid-column: 2 / span 8;
+        grid-row: span 3;
+        text-align: left;
+        margin-bottom: 1rem;
+    }
+    .showCard .cardBack{
+        display: flex;
+        transition: .75s ease-in-out;
+    }
+
 </style>
